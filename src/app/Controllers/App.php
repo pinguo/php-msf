6e0d8429f8b58acb3e989995ef1799490998bb07
pinguo-xudianyang <xudianyang@camera360.com>
2017-02-10 13:37:00 +0800

    * app demo

--- src/app/Controllers/App.php
+++ src/app/Controllers/App.php
@@ -27,6 +27,16 @@ class App extends Controller
         $this->http_output->end(json_encode($data));
     }
 
+    public function httpTestCurlMulti()
+    {
+        $t1 = microtime(true);
+        $this->AppModel = $this->loader->model('AppModel', $this);
+        $data = $this->AppModel->testCurlMulti();
+        $t2 = microtime(true) - $t1;
+        $data[] = $t2;
+        $this->http_output->end(json_encode($data));
+    }
+
     public function httpTestCoroutine()
     {
         $t1 = microtime(true);
@@ -35,5 +45,43 @@ class App extends Controller
         $t2 = microtime(true) - $t1;
         $data[] = $t2;
         $this->http_output->end(json_encode($data));
+    }
+
+    public function httpNoneNested()
+    {
+        $result = [];
+        $httpClient1 = $this->client->coroutineGetHttpClient('http://rec-dev.camera360.com:80');
+        $httpClient2 = $this->client->coroutineGetHttpClient('http://rec-dev.camera360.com:80');
+
+        $c1 = yield $httpClient1;
+        $c2 = yield $httpClient2;
+
+        $r1 = $c1->coroutineGet('/');
+        $r2 = $c2->coroutineGet('/');
+
+        $result[] = strlen(yield $r1);
+        $result[] = strlen(yield $r2);
+        
+        $this->http_output->end(json_encode($result));
+    }
+
+    public function httpNested()
+    {
+        $t1 = microtime(true);
+        $this->AppModel = $this->loader->model('AppModel', $this);
+        $result = yield $this->AppModel->nested();
+        $result[] = microtime(true) - $t1;
+
+        $this->http_output->end(json_encode($result));
+    }
+
+    public function httpNested2()
+    {
+        $t1 = microtime(true);
+        $this->AppModel = $this->loader->model('AppModel', $this);
+        $result   = yield $this->AppModel->nested2();
+        $result[] = microtime(true) - $t1;
+
+        $this->http_output->end(json_encode($result));
     }
 }
