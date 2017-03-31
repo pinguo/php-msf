9f2e1de63aa83862cb003fab60d5d8f848b1eaab
niulingyun <niulingyun@camera360.com>
2017-03-31 14:49:25 +0800

    redis AOP support

--- src/Server/CoreBase/AOPFactory.php
+++ src/Server/CoreBase/AOPFactory.php
@@ -8,6 +8,26 @@
 
 namespace PG\MSF\Server\CoreBase;
 
+use PG\MSF\Server\DataBase\CoroutineRedisHelp;
+
 class AOPFactory
 {
+    /**
+     * 获取协程redis
+     * @param CoroutineRedisHelp $redisPoolCoroutine
+     * @param CoreBase $coreBase
+     * @return AOP|CoroutineRedisHelp
+     */
+    public static function getRedisPoolCoroutine(CoroutineRedisHelp $redisPoolCoroutine, CoreBase $coreBase)
+    {
+        $redisPoolCoroutine = new AOP($redisPoolCoroutine);
+        $redisPoolCoroutine->registerOnBefore(function ($method, $arguments) use ($coreBase) {
+            $context = $coreBase->getContext();
+            array_unshift($arguments, $context);
+            $data['method'] = $method;
+            $data['arguments'] = $arguments;
+            return $data;
+        });
+        return $redisPoolCoroutine;
+    }
 }
