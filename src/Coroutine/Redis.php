934861ebe34bc2c5b592b40141791b5e3948d65d
niulingyun <niulingyun@camera360.com>
2017-05-15 19:15:26 +0800

    redis 支持 keyPrefix 序列化  hashKey

--- src/Coroutine/Redis.php
+++ src/Coroutine/Redis.php
@@ -20,22 +20,38 @@ class Redis extends Base
     public $name;
     public $arguments;
 
+    public $serializer = null;
+
     public function __construct(Context $context, $redisAsynPool, $name, $arguments)
     {
         parent::__construct(3000);
         $this->redisAsynPool = $redisAsynPool;
-        $this->name          = $name;
-        $this->arguments     = $arguments;
-        $this->request       = "redis.$name";
+        $this->serializer = $redisAsynPool->serializer;
+        $this->name = $name;
+        $this->arguments = $arguments;
+        $this->request = "redis.$name";
 
         $context->getLog()->profileStart($this->request);
         getInstance()->coroutine->IOCallBack[$context->getLogId()][] = $this;
-        $this->send(function ($result) use ($context) {
+        $this->send(function ($result) use ($name, $context) {
             if (empty(getInstance()->coroutine->taskMap[$context->getLogId()])) {
                 return;
             }
 
             $context->getLog()->profileEnd($this->request);
+
+            // return value unserialize start
+            if (in_array($name, ['get'])) {
+                $result = $this->unSerializeHandler($result);
+            } elseif (in_array($name, ['mget'])) {
+                $newValues = [];
+                foreach ($result as $k => $v) {
+                    $newValues[$k] = $this->unSerializeHandler($v);
+                }
+                $result = $newValues;
+            }
+            // return value unserialize end
+
             $this->result = $result;
             $this->ioBack = true;
             $this->nextRun($context->getLogId());
@@ -53,5 +69,19 @@ class Redis extends Base
         unset($this->redisAsynPool);
         unset($this->name);
         unset($this->arguments);
+    }
+
+    /**
+     * 反序列化
+     * @param $data
+     * @return mixed
+     */
+    protected function unSerializeHandler($data)
+    {
+        if ($this->serializer == \Redis::SERIALIZER_PHP) {
+            return unserialize($data);
+        } else {
+            return $data;
+        }
     }
 }
