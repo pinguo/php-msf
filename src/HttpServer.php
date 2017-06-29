dfd199e4a25b6e5d5586d70394ab236c0449ba48
pinguo-xudianyang <xudianyang@camera360.com>
2017-06-29 10:55:23 +0800

    * 修复直接访问/的bug

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -137,11 +137,11 @@ abstract class HttpServer extends Server
         do {
             if ($this->route->getPath() == '') {
                 list($host) = explode(':', $request->header['host'] ?? '');
-                $wwwPath = $this->getHostRoot($host) . $this->getHostIndex($host);
-                $result = httpEndFile($wwwPath, $request, $response);
+                $wwwPath    = $this->getHostRoot($host) . $this->getHostIndex($host);
+                $result     = httpEndFile($wwwPath, $request, $response);
                 if (!$result) {
                     $error = 'Index not found';
-                    $code = 404;
+                    $code  = 404;
                 }
                 break;
             }
