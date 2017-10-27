906c8318b5007e77a81d2f52d6d409244a165608
niulingyun <niulingyun@camera360.com>
2017-10-27 17:19:09 +0800

    add RabbitMQ simple support, need do more

---
+++ src/Queue/RabbitMQ.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Queue RabbitMQ
+ * @author camera360_server@camera360.com
+ * @copyright Chengdu pinguo Technology Co.,Ltd.
+ * Date: 26/10/2017
+ */
+
+namespace PG\MSF\Queue;
+
+use PG\MSF\Base\Core;
+use PG\MSF\Tasks\AMQPTask;
+
+class RabbitMQ extends Core implements IQueue
+{
+    /** @var mixed|AMQPTask|\stdClass */
+    public $rabbit;
+
+    public function __construct($configKey, $exchange = 'default', $routing_key = 'default')
+    {
+        $this->rabbit = $this->getObject(AMQPTask::class, [$configKey, $exchange, $routing_key]);
+    }
+
+    /**
+     * 入队
+     * @param string $data
+     * @param string $queue
+     * @return bool
+     */
+    public function set(string $data, string $queue = 'default')
+    {
+        return $this->rabbit->publish($data, $queue);
+    }
+
+    /**
+     * 出队
+     * @param string $queue
+     * @param bool $isAck
+     * @return \AMQPEnvelope|string
+     */
+    public function get(string $queue = 'default', $isAck = true)
+    {
+        /** @var \AMQPEnvelope $AMQPEnvelope */
+        $AMQPEnvelope = yield $this->rabbit->get($isAck);
+        if (!is_object($AMQPEnvelope)) {
+            return $AMQPEnvelope;
+        }
+        return $AMQPEnvelope->getBody();
+    }
+}
